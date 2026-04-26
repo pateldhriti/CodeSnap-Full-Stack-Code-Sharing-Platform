2b0098e8c6321791c933f4cb9737fba015aea401
pateldhriti <pdhriti489@gmail.com>
2026-04-26 13:40:03 -0400

    feat: implement core modules

---
+++ lib/auth.ts
@@ -0,0 +1,91 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { compare } from "bcrypt";
+import GoogleProvider from "next-auth/providers/google";
+
+import { db } from "@/lib/db";
+
+export const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(db),
+    secret: process.env.NEXT_AUTH_SECRET,
+    session: {
+        strategy: "jwt",
+    },
+    pages: {
+        signIn: "/sign-in",
+        error: "/auth-error",
+    },
+    providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                email: {
+                    label: "Email",
+                    type: "email",
+                },
+                password: { label: "Password", type: "password" },
+            },
+            async authorize(credentials) {
+                if (!credentials?.email || !credentials.password) {
+                    return null;
+                }
+
+                const existingUser = await db.user.findUnique({
+                    where: {
+                        email: credentials?.email,
+                    },
+                });
+
+                if (!existingUser) {
+                    return null;
+                }
+
+                if (!existingUser.password) {
+                    return null;
+                }
+
+                const passwordMatch = await compare(
+                    credentials.password,
+                    existingUser.password,
+                );
+
+                if (!passwordMatch) {
+                    return null;
+                }
+
+                return {
+                    id: `${existingUser.id}`,
+                    name: existingUser.name,
+                    email: existingUser.email,
+                };
+            },
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID as string,
+            clientSecret: process.env.GOOGLE_SECRET as string,
+            allowDangerousEmailAccountLinking: true,
+            httpOptions: {
+                timeout: 40000,
+            },
+        }),
+    ],
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user) {
+                return {
+                    ...token,
+                    name: user.name,
+                };
+            }
+
+            return token;
+        },
+        async session({ session, token }) {
+            return {
+                ...session,
+                user: { ...session.user, name: token.name, id: token.sub },
+            };
+        },
+    },
+};
